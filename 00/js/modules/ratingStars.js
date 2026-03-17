face97850bd80ce27cec983b242b5a208a4439b8
gh-deploy <deploy@shoptet.cz>
2026-03-17 08:23:25 +0100

    Update

--- 00/js/modules/ratingStars.js
+++ 00/js/modules/ratingStars.js
@@ -1,12 +1,12 @@
 // @ts-check
 
 import { ensure, ensureEvery, isHTMLFormElement, isHTMLInputElement, maybe } from '../../../shared/js/typeAssertions';
+import { resolvePrefillValues } from './globalFunctions';
 
 const voteForm = maybe(document.querySelector('#formRating'), isHTMLFormElement);
 // @ts-expect-error Shoptet object types are not defined
 if (voteForm && shoptet.config.discussion_rating_forms) {
   // TODO: Remove the ums condition above in issue #20873
-  const search = window.location.search;
   const emailInput = ensure(voteForm.querySelector('input[name="email"]'), isHTMLInputElement);
   const buyerNameInput = ensure(voteForm.querySelector('input[name="fullName"]'), isHTMLInputElement);
   const scoreInputs = ensureEvery(Array.from(voteForm.querySelectorAll('input[name="score"]')), isHTMLInputElement);
@@ -32,25 +32,20 @@ if (voteForm && shoptet.config.discussion_rating_forms) {
     });
   });
 
-  if (search.length > 0) {
-    const searchValues = new URLSearchParams(search);
-    const email = searchValues.get('email');
-    const buyerName = searchValues.get('buyerName');
-    const preselectStars = searchValues.get('preselectStars');
+  const { email, fullName, preselectStars } = resolvePrefillValues();
 
-    if (email || buyerName || preselectStars) {
-      document.querySelector('.rate-wrapper .vote-form')?.classList.add('visible');
-      document.querySelector('.rate-wrapper.unveil-wrapper')?.classList.add('unveiled');
-    }
+  if (email || fullName || preselectStars) {
+    document.querySelector('.rate-wrapper .vote-form')?.classList.add('visible');
+    document.querySelector('.rate-wrapper.unveil-wrapper')?.classList.add('unveiled');
+  }
 
-    if (email) {
-      emailInput.value = email;
-    }
-    if (buyerName) {
-      buyerNameInput.value = buyerName;
-    }
-    if (preselectStars) {
-      scoreInputs.find(input => input.value === preselectStars)?.parentElement?.click();
-    }
+  if (email) {
+    emailInput.value = email;
+  }
+  if (fullName) {
+    buyerNameInput.value = fullName;
+  }
+  if (preselectStars) {
+    scoreInputs.find(input => input.value === preselectStars)?.parentElement?.click();
   }
 }
